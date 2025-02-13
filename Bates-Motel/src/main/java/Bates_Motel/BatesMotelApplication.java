d263296899dea5642fe0f6efa914803087239ff6
FatiguedEndUser <tylor.magadan@protonmail.com>
2025-02-13 14:43:12 -0600

    Added Project Folder pre configed with springboot

---
+++ Bates-Motel/src/main/java/Bates_Motel/BatesMotelApplication.java
@@ -0,0 +1,13 @@
+package Bates_Motel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatesMotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatesMotelApplication.class, args);
+	}
+
+}
---
+++ Bates-Motel/src/test/java/Bates_Motel/BatesMotelApplicationTests.java
@@ -0,0 +1,13 @@
+package Bates_Motel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatesMotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
