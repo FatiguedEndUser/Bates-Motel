ce62e19c2a308891fb083469f7ebec821febc89c
FatiguedEndUser <tylor.magadan@protonmail.com>
2025-02-25 12:12:48 -0600

    Removed some dev tools because it added docker and that was causing issues. Also put a temp flag up to remove Datasource configuration and now the project runs. Also added AWS dependencies

--- Bates-Motel/src/main/java/Bates_Motel/Application.java
+++ Bates-Motel/src/main/java/Bates_Motel/Application.java
@@ -5,9 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 /*----------------------------------------------------------------------------------*/
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
