30954c771d6a3815cc4ecf27b01636920704d7d4
FatiguedEndUser <tylor.magadan@protonmail.com>
2025-02-27 14:20:44 -0600

    Removed other 2 main files and put the commandline runner in the BatesMotelApplication

--- src/main/java/dev/besharps/batesmotel/BatesMotelApplication.java
+++ src/main/java/dev/besharps/batesmotel/BatesMotelApplication.java
@@ -1,11 +1,15 @@
 package dev.besharps.batesmotel;
 
 /*------------------------------------------IMPORTS---------------------------------*/
+import dev.besharps.batesmotel.DB.Student;
+import dev.besharps.batesmotel.DB.StudentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 /*----------------------------------------------------------------------------------*/
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
@@ -15,6 +19,19 @@ public class BatesMotelApplication {
     public static void main(String[] args) {
         SpringApplication.run(BatesMotelApplication.class, args);
         log.info("Application started succesfully");
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student maria = new Student(
+                    "Maria",
+                    "Jones",
+                    "maria.jones@gmail.com",
+                    21
+            );
+            studentRepository.save(maria);
+        };
 
     }
 }
