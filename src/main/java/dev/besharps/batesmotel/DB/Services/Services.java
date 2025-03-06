c9897c8acc4dd53da199737d4cf1bb9bdb33b18d
AbdulahiDirie <xi8546wi@go.minnstate.edu>
2025-03-06 14:50:47 -0600

    added code database files

--- src/main/java/dev/besharps/batesmotel/DB/Services/Services.java
+++ src/main/java/dev/besharps/batesmotel/DB/Services/Services.java
@@ -1,4 +1,37 @@
 package dev.besharps.batesmotel.DB.Services;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "Services")
+@Table(name = "Services")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@ToString
 public class Services {
+    @Id
+    @SequenceGenerator(
+            name = "service_sequence",
+            sequenceName = "service_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "service_sequence"
+    )
+    @Column(
+            name = "serviceId",
+            updatable = false
+    )
+    private int serviceId;
+
+    @Column(
+            name = "serviceCharge",
+            updatable = true,
+            nullable = false
+    )
+    private double serviceCharge;
 }
