b914e32682560b7d324b67cbffc159e339518803
jacob <jacob.thelen@go.dctc.edu>
2025-03-22 12:59:07 -0500

    Added UserTypeTest.java to ensure test getUserTypeByName returns userType Added a switch to the createUser function in UserService.java to ensure it uses at least the 5 defined roles

--- src/main/java/dev/besharps/batesmotel/DB/UserType/UserTypeRepository.java
+++ src/main/java/dev/besharps/batesmotel/DB/UserType/UserTypeRepository.java
@@ -2,10 +2,11 @@ package dev.besharps.batesmotel.DB.UserType;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 
+@Repository
 public interface UserTypeRepository extends JpaRepository<UserType, Integer> {
     List<UserType> getUserTypeByTypeName(String typeName);
 }
