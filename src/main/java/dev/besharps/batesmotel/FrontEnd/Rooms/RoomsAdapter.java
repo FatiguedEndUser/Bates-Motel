e2625c02e961dab3af78b75ad408d7e4dcfcecbe
FatiguedEndUser <tylor.magadan@protonmail.com>
2025-03-23 12:58:20 -0500

    ..

--- src/main/java/dev/besharps/batesmotel/FrontEnd/Rooms/RoomsAdapter.java
+++ src/main/java/dev/besharps/batesmotel/FrontEnd/Rooms/RoomsAdapter.java
@@ -1,10 +1,5 @@
 package dev.besharps.batesmotel.FrontEnd.Rooms;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/Rooms-Standard")
 public class RoomsAdapter {
-    public String RoomAdapter() {
-        return "StandardRoom";
-    }
+
 }
