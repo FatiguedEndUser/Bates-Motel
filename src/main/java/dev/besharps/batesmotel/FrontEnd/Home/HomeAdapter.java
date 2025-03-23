ac777ae8cb18fb0644c98f3f2d3ac84920a3aab0
FatiguedEndUser <tylor.magadan@protonmail.com>
2025-03-23 12:03:00 -0500

    Added mapping for web pages as well as removed test.css and connected db to local machine

--- src/main/java/dev/besharps/batesmotel/FrontEnd/Home/HomeAdapter.java
+++ src/main/java/dev/besharps/batesmotel/FrontEnd/Home/HomeAdapter.java
@@ -1,5 +1,12 @@
 package dev.besharps.batesmotel.FrontEnd.Home;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
 public class HomeAdapter {
 
+    public String HomeAdapter() {
+        return "HomePage";
+    }
 }
