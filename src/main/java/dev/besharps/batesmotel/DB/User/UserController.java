c80d9b705fc4fa97cd087051cc5ce7f66281b7f7
jacob <jacob.thelen@go.dctc.edu>
2025-03-24 10:24:01 -0500

    Added into UserType.java @JsonBackReference to OneToOne mapping to the User to prevent "infinite json looping"
    Added mapping into UserController.java to fetch all users

--- src/main/java/dev/besharps/batesmotel/DB/User/UserController.java
+++ src/main/java/dev/besharps/batesmotel/DB/User/UserController.java
@@ -1,5 +1,26 @@
 package dev.besharps.batesmotel.DB.User;
 
+import dev.besharps.batesmotel.DB.Customer.Customer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
-  
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
 }
