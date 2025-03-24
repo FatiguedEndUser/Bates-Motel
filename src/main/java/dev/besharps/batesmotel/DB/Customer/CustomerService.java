c9b8c37943ee555f0fbb35293e37d6702aa12553
jacob <jacob.thelen@go.dctc.edu>
2025-03-23 21:36:20 -0500

    Finished the updateCustomer fields in CustomerService.java to include all updatable fields Commented out @EntityScan and @EnableJpaRepositories Added another test function into CustomerRepositoryTest.java

--- src/main/java/dev/besharps/batesmotel/DB/Customer/CustomerService.java
+++ src/main/java/dev/besharps/batesmotel/DB/Customer/CustomerService.java
@@ -1,28 +1,42 @@
 package dev.besharps.batesmotel.DB.Customer;
 
+import jakarta.transaction.Transactional;
+import org.hibernate.service.spi.InjectService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Service
 public class CustomerService {
-    private final CustomerRepository customerRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
 
-    public CustomerService(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
-
-    public void updateCustomer(int customerId, String firstName, String lastName) {
+    @Transactional
+    public void updateCustomer(int customerId, String firstName, String lastName, String address, String phone, String carinformation) {
         Customer customer = customerRepository.findById(customerId).orElse(null);
         if (customer == null) {
             throw new IllegalArgumentException("Customer not found");
         }
 
-        customer.setFirstName(firstName);
-        customer.setLastName(lastName);
+        // Maybe a better way to do this?
+        if (firstName != null) {
+            customer.setFirstName(firstName);
+        }
+        if (lastName != null) {
+            customer.setLastName(lastName);
+        }
+        if (address != null) {
+            customer.setAddress(address);
+        }
+        if (phone != null) {
+            customer.setPhoneNumber(phone);
+        }
+        if (carinformation != null) {
+            customer.setCarInformation(carinformation);
+        }
 
         customerRepository.save(customer);
-
-
     }
 }
