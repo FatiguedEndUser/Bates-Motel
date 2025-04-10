d218c9a02fc4489b1e713ea32013f6760ab110b0
AbdulahiDirie <xi8546wi@go.minnstate.edu>
2025-04-10 13:37:07 -0500

    test

--- src/main/java/dev/besharps/batesmotel/DB/Bookings/Bookings.java
+++ src/main/java/dev/besharps/batesmotel/DB/Bookings/Bookings.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Getter
 @ToString
 @Builder
-public class Bookings {
+public class lBookings {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "bookingId", nullable = false, updatable = false)
