b9c2037d102da7f8fa37b7b6dc64727fdc845bb5
FatiguedEndUser <tylor.magadan@protonmail.com>
2025-04-16 13:38:01 -0500

    Added model and attribute for dashboard but working through Database grabs right now

--- src/main/java/dev/besharps/batesmotel/FrontEnd/Dashboard/DashboardMapping.java
+++ src/main/java/dev/besharps/batesmotel/FrontEnd/Dashboard/DashboardMapping.java
@@ -1,12 +1,47 @@
 package dev.besharps.batesmotel.FrontEnd.Dashboard;
 
+import dev.besharps.batesmotel.DB.Bookings.Bookings;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardMapping {
-    public String Dashboard(){
+
+    @GetMapping
+    public String Dashboard(Model model,
+                            @RequestParam Bookings bookings,
+                            @RequestParam(name = "id") String bookingId,
+                            @RequestParam LocalDate startDate,
+                            @RequestParam LocalDate endDate,
+                            @RequestParam Bookings room){
+
+        model.addAttribute("bookings", bookings);
+        model.addAttribute("room", room);
+        model.addAttribute("startDate", startDate);
+        model.addAttribute("endDate", endDate);
+        model.addAttribute("bookingId", bookingId);
+
         return "Dashboard";
+    }
+
+    public void populateDashboard(Model model,
+                                  @RequestParam String firstName,
+                                  @RequestParam String lastName,
+                                  @RequestParam String email,
+                                  @RequestParam int loyaltyPoints,
+                                  @RequestParam int id){
+        model.addAttribute("firstName", firstName);
+        model.addAttribute("lastName", lastName);
+        model.addAttribute("email", email);
+        model.addAttribute("loyaltyPoints", loyaltyPoints);
+        model.addAttribute("id", id);
+
+
     }
 }
